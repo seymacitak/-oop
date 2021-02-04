828439a47ced5ece20b11263cd42dcac0bb54899
Seyma Citak <citak.seymaa@gmail.com>
2021-02-04 18:29:20 +0300

    animal class template created

---
+++ animal.py
@@ -0,0 +1,18 @@
+class Animal():
+    def __init__(self, name, lifespan, class_type, weight, age, nutrition ): 
+        print("init function of animal class")
+        self.lifespan = lifespan
+        self.class_type = class_type
+        self.weight = weight
+        self.age = age
+        self.nutrition = nutrition 
+
+
+
+
+
+
+
+
+
+
